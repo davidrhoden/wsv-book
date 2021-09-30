a416c45b576347f4fbc5ec8f75faa0b9f4fb7a7a
David Rhoden <david@davidrhoden.com>
2021-09-29 19:31:57 -0500

    do you stage migrations?

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+from .models import Post
 from django.views.generic import TemplateView
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+	model = Post
 	template_name = 'home.html'
+	context_object_name = 'all_posts_list'
 
 class AboutPageView(TemplateView):
 	template_name = 'about.html'
